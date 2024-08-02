e658440e751557b6904195beb9d805957f833c82
PAMBAK-AAT <22bme064@nith.ac.in>
2024-08-02 17:45:55 +0530

    url change

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,4 @@
 
-
-
-
-
 import React, { useEffect, useState } from 'react';
 import { IoChatbubbleEllipses } from "react-icons/io5";
 import { FaUserPlus } from "react-icons/fa";
@@ -14,11 +10,11 @@ import EditUserDetails from './EditUserDetails.jsx';
 import { GoArrowUpLeft } from "react-icons/go";
 import SearchUser from './SearchUser.jsx';
 import { FaImage, FaVideo } from "react-icons/fa";
-import { logout, setSocketConnection } from '../redux/userSlice.jsx';
+import { logout , setSocketConnection } from '../redux/userSlice.jsx';
 
 export const Sidebar = () => {
     const user = useSelector(state => state?.user);
-    const [editUserOpen, setEditUserOpen] = useState(false);
+    const [editUserOpen, setEditUserOpen] = useState(false); 
     const [allUser, setAllUser] = useState([]);
     const [openSearchUser, setOpenSearchUser] = useState(false);
     const socketConnection = useSelector(state => state?.user?.socketConnection);
@@ -30,14 +26,14 @@ export const Sidebar = () => {
         if (storedUser) {
             dispatch(setSocketConnection(storedUser.socketConnection));
         }
-
+        
         if (socketConnection) {
             console.log('Connected to socket server');
             socketConnection.emit('sidebar', user?._id);
-
+    
             socketConnection.on("conversation", (data) => {
                 console.log('Received conversation data:', data);
-
+    
                 const conversationUserData = data.map((conversationUser, index) => {
                     if (conversationUser.sender?._id === user?._id) {
                         return {
@@ -51,115 +47,138 @@ export const Sidebar = () => {
                         };
                     }
                 });
-
+    
                 console.log('Processed conversationUserData:', conversationUserData);
                 setAllUser(conversationUserData);
             });
         }
     }, [socketConnection, user]);
-
+    
     const handleLogOut = () => {
-        dispatch(logout());
+        dispatch(logout())
         navigate("/email");
-        localStorage.removeItem('user');
+        localStorage.removeItem('user'); // Clear user data from localStorage
         localStorage.clear();
-    };
+    }
 
     return (
         <div className='h-full flex'>
-            <div className="bg-slate-100 w-16 h-full py-4 flex flex-col justify-between fixed z-10 shadow-lg">
+            <div className="bg-slate-100 w-16 h-full py-4 flex flex-col justify-between fixed z-10">
                 <div>
-                    <NavLink to="/" className={({ isActive }) => `w-12 h-12 my-2 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded-full ${isActive && 'bg-slate-300'}`} title='chat'>
+                    <NavLink to="/" className={({ isActive }) => `w-12 h-12 my-1 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && 'bg-slate-300'}`} title='chat'>
                         <IoChatbubbleEllipses size={30} />
                     </NavLink>
 
-                    <div title="add friends" onClick={() => setOpenSearchUser(true)} className="w-12 h-12 my-2 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded-full">
+                    <div title="add friends" onClick={() => setOpenSearchUser(true)} className="w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded">
                         <FaUserPlus size={30} />
                     </div>
                 </div>
 
                 <div className="flex flex-col items-center">
-                    <button className='m-3' title={user.name} onClick={() => setEditUserOpen(true)}>
+                    <button className='mx-auto my-3' title={user.name} onClick={() => setEditUserOpen(true)}>
                         <Profile
                             width={50}
-                            height={70}
+                            height={50}
                             name={user?.name}
                             imageUrl={user?.profile_pic}
                             userId={user?._id}
                         />
                     </button>
 
-                    <button title="logout" className="w-10 h-10 my-2 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded-full" onClick={handleLogOut}>
+                    <button title="logout" className="w-10 h-10 my-3 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded" onClick={handleLogOut}>
                         <BiLogOut size={30} />
                     </button>
                 </div>
             </div>
 
-            <div className='w-full ml-16 relative z-20'>
+            <div className='w-full ml-[5rem] relative z-20'>
                 <div className='h-20 flex items-center'>
                     <h2 className='text-xl font-bold p-6 mt-5 text-slate-800'>Message</h2>
                 </div>
 
-                <div className='bg-slate-300 p-[0.6px]'></div>
+                <div className='bg-slate-300 h-[2px]'></div>
 
-                <div className='h-[calc(100vh-85px)] overflow-x-hidden overflow-y-auto'>
-                    {allUser.length === 0 ? (
-                        <div className='mt-12 p-3'>
-                            <div className='flex justify-center items-center text-slate-500'>
-                                <GoArrowUpLeft size={45} />
+                <div className='h-[calc(100vh-85px)] overflow-x-hidden overflow-y-auto scrollbar'>
+                    {
+                        allUser.length === 0 && (
+                            <div className='mt-12 p-3'>
+                                <div className='flex justify-center items-center text-slate-500'>
+                                    <GoArrowUpLeft size={45} />
+                                </div>
+                                <p className='text-lg text-center font-semibold text-slate-500'>Explore users to start a conversation with.</p>
                             </div>
-                            <p className='text-lg text-center font-semibold text-slate-500'>Explore users to start a conversation with.</p>
-                        </div>
-                    ) : (
-                        allUser.map((conv, index) => (
-                            <NavLink
-                                to={"/" + conv?.userDetails?._id}
-                                key={conv?._id}
-                                className='flex items-center gap-4 p-4 border border-transparent hover:border-primary hover:bg-gray-100 rounded-lg transition-colors duration-300 ease-in-out shadow-lg'
-                            >
-                                <div className='flex-shrink-0'>
-                                    <Profile
-                                        imageUrl={conv?.userDetails?.profile_pic}
-                                        name={conv?.userDetails?.name}
-                                        height={60}
-                                        width={60}
-                                        userId={conv?.userDetails?._id}
-                                        className='rounded-full border border-gray-300'
-                                    />
-                                </div>
-                                <div className='flex-1'>
-                                    <h3 className='text-xl font-semibold text-gray-800 truncate'>{conv?.userDetails?.name}</h3>
-                                    <div className='text-sm text-gray-500 flex items-center gap-2 mt-1'>
-                                        <div className='flex items-center gap-2'>
-                                            {conv?.lastMsg?.imageUrl && (
-                                                <div className='flex items-center gap-1'>
-                                                    <FaImage className='text-gray-600' />
-                                                    {!conv?.lastMsg?.text && <span>Image</span>}
-                                                </div>
-                                            )}
-                                            {conv?.lastMsg?.videoUrl && (
-                                                <div className='flex items-center gap-1'>
-                                                    <FaVideo className='text-gray-600' />
-                                                    {!conv?.lastMsg?.text && <span>Video</span>}
-                                                </div>
-                                            )}
-                                        </div>
-                                        <p className='text-gray-700 truncate'>{conv?.lastMsg?.text}</p>
+                        )
+                    }
+
+                    {
+                        allUser.map((conv, index) => {
+                            console.log('Rendering Profile with:', conv?.userDetails);
+                            return (
+                                <NavLink
+                                    to={"/" + conv?.userDetails?._id}
+                                    key={conv?._id}
+                                    className='flex items-center gap-2 p-1 border border-transparent hover:border-primary hover:bg-gray-100 rounded-lg transition-colors duration-300 ease-in-out'
+                                >
+                                    <div className='flex-shrink-0'>
+                                        <Profile
+                                            imageUrl={conv?.userDetails?.profile_pic}
+                                            name={conv?.userDetails?.name}
+                                            height={50}
+                                            width={50}
+                                            userId={conv?.userDetails?._id}
+                                            className='rounded-full border border-gray-300'
+                                        />
                                     </div>
-                                </div>
-                                {Boolean(conv?.unseenMsg) && (
-                                    <p className='text-xs font-bold bg-primary text-white w-6 h-6 flex justify-center items-center rounded-full'>
-                                        {conv?.unseenMsg}
-                                    </p>
-                                )}
-                            </NavLink>
-                        ))
-                    )}
+                                    <div className='flex-1'>
+                                        <h3 className='text-lg font-semibold text-gray-800 truncate'>{conv?.userDetails?.name}</h3>
+                                        <div className='text-sm text-gray-500 flex items-center gap-2 mt-1'>
+                                            <div className='flex items-center gap-2'>
+                                                {
+                                                    conv?.lastMsg?.imageUrl && (
+                                                        <div className='flex items-center gap-1'>
+                                                            <FaImage className='text-gray-600' />
+                                                            {!conv?.lastMsg?.text && <span>Image</span>}
+                                                        </div>
+                                                    )
+                                                }
+                                                {
+                                                    conv?.lastMsg?.videoUrl && (
+                                                        <div className='flex items-center gap-1'>
+                                                            <FaVideo className='text-gray-600' />
+                                                            {!conv?.lastMsg?.text && <span>Video</span>}
+                                                        </div>
+                                                    )
+                                                }
+                                            </div>
+                                            <p className='text-gray-700 truncate'>{conv?.lastMsg?.text}</p>
+                                        </div>
+                                    </div>
+                                    {
+                                        Boolean(conv?.unseenMsg) && (
+                                            <p className='text-xs font-bold bg-primary text-white w-6 h-6 flex justify-center items-center rounded-full'>
+                                                {conv?.unseenMsg}
+                                            </p>
+                                        )
+                                    }
+                                </NavLink>
+                            );
+                        })
+                    }
+
                 </div>
             </div>
 
-            {editUserOpen && <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />}
-            {openSearchUser && <SearchUser onClose={() => setOpenSearchUser(false)} />}
+            {
+                editUserOpen && (
+                    <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />
+                )
+            }
+
+            {
+                openSearchUser && (
+                    <SearchUser onClose={() => setOpenSearchUser(false)} />
+                )
+            }
         </div>
     );
 };
@@ -298,7 +317,7 @@ export const Sidebar = () => {
 //                                 <NavLink
 //                                     to={"/" + conv?.userDetails?._id}
 //                                     key={conv?._id}
-//                                     className='flex items-center gap-4 p-4 border border-transparent hover:border-primary hover:bg-gray-100 rounded-lg transition-colors duration-300 ease-in-out shadow-lg'
+//                                     className='flex items-center gap-2 p-1 border border-transparent hover:border-primary hover:bg-gray-100 rounded-lg transition-colors duration-300 ease-in-out '
 //                                 >
 //                                     <div className='flex-shrink-0'>
 //                                         <Profile

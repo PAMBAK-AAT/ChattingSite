e314c53300661ff910a32dbeccea62282cf7c519
PAMBAK-AAT <22bme064@nith.ac.in>
2024-08-02 12:36:35 +0530

    url change

--- socket/index.js
+++ socket/index.js
@@ -10,7 +10,7 @@ const app = express();
 const getConversation = require('../helper/getConversation');
 const { ConversationModel, MessageModel } = require('../models/conversationModel');
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
